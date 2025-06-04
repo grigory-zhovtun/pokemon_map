23b8a3fc0615c984c85ef00c5176e78e7c584d90
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-06-04 10:21:22 +0500

    add PokemonEntity model.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,3 +6,12 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    latitude = models.FloatField(verbose_name='Lat')
+    longitude = models.FloatField(verbose_name='Lon')
+
+    def __str__(self):
+        return f'{self.pokemon.title} ({self.latitude}, {self.longitude})'
