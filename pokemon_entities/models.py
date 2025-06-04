e3976922f7edd0b79901a92c43b02c6846e51adf
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-06-04 21:32:41 +0500

    Add relation to db models.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
