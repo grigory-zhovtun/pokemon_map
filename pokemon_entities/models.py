134f4406741b5a8b0e5f7d7d840278f66137d9ab
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-06-06 19:18:00 +0500

    add verbose_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -18,23 +18,23 @@ class Pokemon(models.Model):
         verbose_name="Предыдущая эволюция"
     )
 
-
-def __str__(self):
+    def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    latitude = models.FloatField(verbose_name='Lat')
-    longitude = models.FloatField(verbose_name='Lon')
-    appeared_at = models.DateTimeField(verbose_name='Appeared at', null=True, blank=True)
-    disappeared_at = models.DateTimeField(verbose_name='Disappeared at', null=True, blank=True)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name="Покемон")
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
+    appeared_at = models.DateTimeField(verbose_name='Время появления', null=True, blank=True)
+    disappeared_at = models.DateTimeField(verbose_name='Время исчезновения', null=True, blank=True)
 
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defense = models.IntegerField()
-    stamina = models.IntegerField()
+    level = models.IntegerField(verbose_name="Уровень")
+    health = models.IntegerField(verbose_name="Здоровье")
+    strength = models.IntegerField(verbose_name="Сила")
+    defense = models.IntegerField(verbose_name="Защита")
+    stamina = models.IntegerField(verbose_name="Выносливость")
 
-    def __str__(self):
+
+def __str__(self):
         return f'{self.pokemon.title} ({self.latitude}, {self.longitude})'
